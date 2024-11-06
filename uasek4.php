fdc1c36432f4ac6a5b6d2c62baae67d831bd39af
Faiz-Liners <faizuddin0000@gmail.com>
2024-11-06 21:09:14 +0800

    uasek4.php

---
+++ uasek4.php
@@ -0,0 +1,336 @@
+##### SOURCE : https://github.com/luutuyen2k9/iptv-vn
+##### VIET CHANNEL
+
+#EXTM3U url-tvg="http://lichphatsong.xyz/schedule/epg.xml.gz"
+
+#EXTINF:-1 tvg-id="vtv1hd" group-title="Kênh miễn phí", VTV1
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtv1/index.m3u8
+
+#EXTINF:-1 tvg-id="vtv2hd" group-title="Kênh miễn phí", VTV2
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv2hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv3hd" group-title="Kênh miễn phí", VTV3
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv3hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv4hd" group-title="Kênh miễn phí", VTV4
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtv4/index.m3u8
+
+#EXTINF:-1 tvg-id="vtv5hd" group-title="Kênh miễn phí", VTV5
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv5hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv6hd" group-title="Kênh miễn phí", VTV Can Tho
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv6hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv7hd" group-title="Kênh miễn phí", VTV7
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv7hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv8hd" group-title="Kênh miễn phí", VTV8
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv8hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv9hd" group-title="Kênh miễn phí", VTV9
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv9hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv5hdtnb" group-title="Kênh miễn phí", VTV5 Tay Nam Bo
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv5hdtnb/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtv5hdtn" group-title="Kênh miễn phí", VTV5 Tay Nguyen
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtv5hdtn/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtc1hd" group-title="Kênh miễn phí", VTC1
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc1/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc2" group-title="Kênh miễn phí", VTC2
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc2/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc3hd" group-title="Kênh miễn phí", VTC3
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc3/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc4hd" group-title="Kênh miễn phí", VTC4
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc4/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc5hd" group-title="Kênh miễn phí", VTC5
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc5/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc6hd" group-title="Kênh miễn phí", VTC6
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc6/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc7hd" group-title="Kênh miễn phí", VTC7
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtc7hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtc8" group-title="Kênh miễn phí", VTC8
+#EXTVLCOPT:http-user-agent=iptv-vn
+http://thvip.site/vtc8/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc9hd" group-title="Kênh miễn phí", VTC9
+#EXTVLCOPT:http-user-agent=iptv-vn
+http://thvip.site/vtc9/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc10" group-title="Kênh miễn phí", VTC10
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtc10/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtc11" group-title="Kênh miễn phí", VTC11
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtc11/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtc14hd" group-title="Kênh miễn phí", VTC14
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc14/index.m3u8
+
+#EXTINF:-1 tvg-id="vtc16hd" group-title="Kênh miễn phí", VTC16
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/vtc16/index.m3u8
+
+#EXTINF:-1 tvg-id="htv1" group-title="Kênh miễn phí", HTV1
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/htv1/index.m3u8
+
+#EXTINF:-1 tvg-id="htv2hd" group-title="Kênh miễn phí", HTV2
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/htv2/index.m3u8
+
+#EXTINF:-1 tvg-id="htv3" group-title="Kênh miễn phí", HTV3
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/htv3/index.m3u8
+
+#EXTINF:-1 tvg-id="htvkey" group-title="Kênh miễn phí", HTV Key
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/htv4/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="htv7hd" group-title="Kênh miễn phí", HTV7
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/htv7/index.m3u8
+
+#EXTINF:-1 tvg-id="htv9hd" group-title="Kênh miễn phí", HTV9
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://thvip.site/htv9/index.m3u8
+
+#EXTINF:-1 tvg-id="htvthethaohd" group-title="Kênh miễn phí", HTV The Thao
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/htvthethaohd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vinhlong1hd" group-title="Kênh miễn phí", THVL1
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/htvl1/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vinhlong2hd" group-title="Kênh miễn phí", THVL2
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/thvl2/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vinhlong3hd" group-title="Kênh miễn phí", THVL3
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/thvl3/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vinhlong4hd" group-title="Kênh miễn phí", THVL4
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/thvl4/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="hanoi1" group-title="Kênh miễn phí", Ha Noi 1
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/121/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="hanoi2" group-title="Kênh miễn phí", Ha Noi 2
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://cloudstreamhntv.tek4tv.vn/live/smil:HN2.smil/chunklist_w209459414_b3128000_slen.m3u8
+
+#EXTINF:-1 tvg-id="daklak" group-title="Kênh miễn phí", DRT TV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://cdn.drt.vn/live/285a27750861b964c27af22091662a74f2f/playlist.m3u8
+
+#EXTINF:-1 tvg-id="haiphong" group-title="Kênh miễn phí", THP
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://live.mediatech.vn/live/285a4c99665fdf84e94956c66bc7dc7eb5d/chunklist.m3u8
+
+#EXTINF:-1 tvg-id="haiphongplus" group-title="Kênh miễn phí", THP+
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://live.mediatech.vn/live/28548ca35823d41426d8b3da7ed82bdab13/chunklist.m3u8
+
+#EXTINF:-1 tvg-id="angiang" group-title="Kênh miễn phí", ATV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://oipf0znpmnliv.vcdn.cloud/hls/thangiang/index.m3u8
+
+#EXTINF:-1 tvg-id="yenbai" group-title="Kênh miễn phí", YTV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://live.yenbaitv.org.vn/hls/livestream.m3u8
+
+#EXTINF:-1 tvg-id="nhandan" group-title="Kênh miễn phí", Nhan Dan TV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://arpt.tvic.vn/live/nhandan720/chunklist_w1856690367.m3u8
+
+#EXTINF:-1 tvg-id="quochoi" group-title="Kênh miễn phí", Quoc Hoi
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://liveh12.vtvcab.vn/hls/ONQUOCHOI_CL/04.m3u8
+
+#EXTINF:-1 tvg-id="vovtvhd" group-title="Kênh miễn phí", VOVTV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://live.mediatech.vn/live/285fbc845578c6641d5a4c40534a0d1864b/chunklist.m3u8
+
+#EXTINF:-1 tvg-id="qpvnhd" group-title="Kênh miễn phí", QPVN
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://liveh34.vtvcab.vn/hls/ONQUOCPHONG_CL/01.m3u8
+
+#EXTINF:-1 tvg-id="antvhd" group-title="Kênh miễn phí", ANTV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://liveh34.vtvcab.vn/hls/ONANTV_CL/04.m3u8
+
+#EXTINF:-1 tvg-id="ttxvnhd" group-title="Kênh miễn phí", VNews
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://liveh34.vtvcab.vn/hls/TTXVN_VNEWS/index.m3u8
+
+#EXTINF:-1 tvg-id="kcinehd" group-title="Kênh trả phí", K+CINE
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tth.vn/Khichtv-live-k-cine-hd-m3u8
+
+#EXTINF:-1 tvg-id="kkidshd" group-title="Kênh trả phí", K+KIDS
+#EXTVLCOPT:http-user-agent=iptv-vn
+http://113.162.2.210:8989/rtp/232.84.3.42:11800
+
+#EXTINF:-1 tvg-id="onsportsplus" group-title="Kênh trả phí", On Sports+
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tth.vn/khích-TiVi-live-vtvcab6.m3u8
+
+#EXTINF:-1 tvg-id="onsports" group-title="Kênh trả phí", On Sports
+#EXTVLCOPT:http-user-agent=iptv-vn
+http://113.162.2.210:8989/rtp/232.84.3.242:11374
+
+#EXTINF:-1 tvg-id="vtvcab1hd" group-title="Kênh trả phí", On Vie Giai Tri
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab1hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab2hd" group-title="Kênh trả phí", On Phim Viet
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab2hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab4hd" group-title="Kênh trả phí", On Movies
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab4hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab5hd" group-title="Kênh trả phí", On E Channel
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab5hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab7hd" group-title="Kênh trả phí", On O2TV
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab7hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab8hd" group-title="Kênh trả phí", On BiBi
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab8hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab9hd" group-title="Kênh trả phí", On Info
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab9hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab10hd" group-title="Kênh trả phí", On Cine
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab10hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab15hd" group-title="Kênh trả phí", On Music
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab15hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab22hd" group-title="Kênh trả phí", On Life
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab22hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="vtvcab23hd" group-title="Kênh trả phí", On Golf
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/vtvcab23hd/KenhTruyenHinhCoBan
+
+#EXTINF:-1 tvg-id="sctv1hd" group-title="Kênh trả phí", SCTV1
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s5.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e8.endpoint.cdn.sctvonline.vn/nginx.s5.edge.cdn.sctvonline.vn/hls/sctv1/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv2hd" group-title="Kênh trả phí", SCTV2
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s5.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e8.endpoint.cdn.sctvonline.vn/nginx.s5.edge.cdn.sctvonline.vn/hls/sctv2/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv3hd" group-title="Kênh trả phí", SCTV3
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s7.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e8.endpoint.cdn.sctvonline.vn/nginx.s7.edge.cdn.sctvonline.vn/hls/sctv3/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv4hd" group-title="Kênh trả phí", SCTV4
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s8.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e8.endpoint.cdn.sctvonline.vn/nginx.s8.edge.cdn.sctvonline.vn/hls/sctv4/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv8hd" group-title="Kênh trả phí", SCTV8
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s1.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e1.endpoint.cdn.sctvonline.vn/nginx.s1.edge.cdn.sctvonline.vn/hls/sctv8/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv16hd" group-title="Kênh trả phí", SCTV16
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s7.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e1.endpoint.cdn.sctvonline.vn/nginx.s7.edge.cdn.sctvonline.vn/hls/sctv16/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv15hd" group-title="Kênh trả phí", SCTV15
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+http://103.171.91.5/play/u2p_e4wGSh0vafm-3bmTbKJiKMjo5cCycWahXI1Ip64/ts
+
+#EXTINF:-1 tvg-id="sctv17hd" group-title="Kênh trả phí", SCTV17
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s10.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e7.endpoint.cdn.sctvonline.vn/nginx.s10.edge.cdn.sctvonline.vn/live/smil:SCTV17.smil/chunklist.m3u8
+
+#EXTINF:-1 tvg-id="sctv22hd" group-title="Kênh trả phí", SCTV22
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://s9.edge.cdn.sctvonline.vn/cdn-cgi/edge/v2/e1.endpoint.cdn.sctvonline.vn/nginx.s9.edge.cdn.sctvonline.vn/hls/sctv22/index.m3u8
+
+#EXTINF:-1 tvg-id="sctv15hd" group-title="Kênh trả phí", SCTV15 [LaLiga Feed]
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://oipf0znqmnliv.pipitv.top/stream/channelid/603100800?profile=pass
+
+#EXTINF:-1 tvg-id="sctv22hd" group-title="Kênh trả phí", SCTV22 [LaLiga Feed]
+#EXTVLCOPT:http-user-agent=ReactNativeVideo/3.4.4 (Linux;Android 9) ExoPlayerLib/2.13.3
+https://oipf0znqmnliv.pipitv.top/stream/channelid/2125266812
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+1
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk1/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+2
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk2/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+3
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk3/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+4
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk4/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+5
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk5/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+6
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk6/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+7
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk7/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+8
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk8/KenhTruyenHinhCoBan
+
+#EXTINF:-1 group-title="Sự kiện trực tiếp", TV360+9
+#EXTVLCOPT:http-user-agent=iptv-vn
+https://tv.TruyenHinh.Click/LinkTongHopCuaMoiNguoi/tv360.sk9/KenhTruyenHinhCoBan
