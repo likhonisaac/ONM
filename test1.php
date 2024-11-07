c080734837099aa8db41e5371777f3810cb5620e
Faiz-Liners <faizuddin0000@gmail.com>
2024-11-08 02:17:32 +0800

    test1.php

---
+++ test1.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+#EXTM3U
+#EXTM3U billed-msg="❤️🧡💛💚 🔰 ONM 🔰 FREE CHANNEL 🔰 💚💛🧡❤️" 
+#EXTM3U url-tvg="https://bit.ly/pondan-epg, https://azimabid00.github.io/epg/astro_epg.xml" refresh="1440" 
+######################################################################################################################################################################################
+
+
+
+######################################################################################################################################################################################
+##########################################################################     FREE CHANNEL     ######################################################################################
+######################################################################################################################################################################################
+
+  
+#####  Inspirasi ##### okay svr1 ## group logo disini ##
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={ "keys":[ { "kty":"oct", "k":"nBBcxoyqnPZTFeBu9Qhgqw", "kid":"GQDQ/Y39I1fKJoXGZii/Bw" } ], "type":"temporary" }
+#EXTINF:-1 tvg-id="Inspirasi" ch-number="001" tvg-name="Inspirasi" group-title="FREE CHANNEL" tvg-logo="https://i.ibb.co/tpJfQ9x/Unifi-Inspirasi.png" group-logo="https://image.similarpng.com/very-thumbnail/2022/01/Abstract-humming-bird-colorful-logo-on-transparent-background-PNG.png",001 Inspirasi Server1
+https://aqfadtv.xyz/live/unifitv/inspirasi/index.mpd
+
+
+##### Inspirasi ##### okay svr2 ##
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={ "keys":[ { "kty":"oct", "k":"nBBcxoyqnPZTFeBu9Qhgqw", "kid":"GQDQ/Y39I1fKJoXGZii/Bw" } ], "type":"temporary" }
+#EXTINF:-1 tvg-id="Inspirasi" ch-number="002" tvg-name="Inspirasi" group-title="FREE CHANNEL" tvg-logo="https://i.ibb.co/tpJfQ9x/Unifi-Inspirasi.png",002 Inspirasi Server2
+https://aqfadtv.xyz/live/unifitv/inspirasi/index.mpd
+
+
+##### Didik TV ##### okay ## ada EPG ##
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key=47a1206b44822c89320e70916cf57b0c:e040ca2aa426df789ba384ba561891b4
+#EXTINF:-1 tvg-id="DidikTV" ch-number="003" tvg-name="DidikTV KPM" group-title="FREE CHANNEL" tvg-logo="https://headend-api.tonton.com.my/v100/imageHelper.php?id=6420324:378:CHANNEL:IMAGE:png&w=200&appID=TONTON",003 Didik TV
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36
+#EXTVLCOPT:http-referrer=https://live-xtra-sg1.global.ssl.fastly.net/embed/index.html?vid=11056641&autoplay=1&mute=0
+https://tonton-live-ssai.akamaized.net/live/0e27ef78-4cc9-4c62-bec9-5c946c9e7354/cmaf.isml/.m3u8
+
+
+##### Salam HD ##### okay ##
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={ "keys":[ { "kty":"oct", "k":"IH86w2yNXIU5XBRxVNQVgQ", "kid":"HNDzPbWoJshQ4O9sqTMagg" } ], "type":"temporary" }
+#EXTINF:-1 tvg-id="Salam" group-title="FREE CHANNEL" tvg-logo="https://playtv.unifi.com.my:7046/CPS/images/universal/film/logo/201907/20190716/20190716062504007e6f.png", 004 Salam HD
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36
+https://unifi-live09.secureswiftcontent.com/UnifiHD/live32.mpd
