c31a7ba356737d81b0e8402416a48009d0d25b7c
Faiz-Liners <faizuddin0000@gmail.com>
2024-11-08 05:06:52 +0800

    menu.js

--- menu.js
+++ menu.js
@@ -1,2 +1,2 @@
-fetch ("https://onm.pages.dev/test1.php");
-fetch ("https://onm.pages.dev/test2.php");
+window.location.href = "https://onm.pages.dev/test1.php";
+window.location.href = "https://onm.pages.dev/test2.php";
