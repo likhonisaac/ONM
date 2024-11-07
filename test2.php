83c2ee5ca828dc4f3303250dd5c8f7ae7551e955
Faiz-Liners <faizuddin0000@gmail.com>
2024-11-08 02:18:29 +0800

    test2.php

---
+++ test2.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+
+#EXTM3U
+#EXTM3U billed-msg="❤️🧡💛💚 🔰 ONM 🔰 FREE CHANNEL 🔰 💚💛🧡❤️" 
+#EXTM3U url-tvg="https://bit.ly/pondan-epg, https://azimabid00.github.io/epg/astro_epg.xml" refresh="1440" 
+######################################################################################################################################################################################
+
+
+
+######################################################################################################################################################################################
+##########################################################################     FREE CHANNEL     ######################################################################################
+######################################################################################################################################################################################
+
+  
+
+
+##### Didik TV ##### okay ## ada EPG ##
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key=47a1206b44822c89320e70916cf57b0c:e040ca2aa426df789ba384ba561891b4
+#EXTINF:-1 tvg-id="DidikTV" ch-number="003" tvg-name="DidikTV KPM" group-title="FREE CHANNEL" tvg-logo="https://headend-api.tonton.com.my/v100/imageHelper.php?id=6420324:378:CHANNEL:IMAGE:png&w=200&appID=TONTON",003 Didik TV
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36
+#EXTVLCOPT:http-referrer=https://live-xtra-sg1.global.ssl.fastly.net/embed/index.html?vid=11056641&autoplay=1&mute=0
+https://tonton-live-ssai.akamaized.net/live/0e27ef78-4cc9-4c62-bec9-5c946c9e7354/cmaf.isml/.m3u8
