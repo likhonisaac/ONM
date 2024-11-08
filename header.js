7124b2d903970077e2c78df88f0a30eaf8ae0590
Faiz-Liners <faizuddin0000@gmail.com>
2024-11-08 14:43:47 +0800

    header.js

---
+++ header.js
@@ -0,0 +1,7 @@
+<!-- Begin
+document.write('<center>');
+document.write('<a href="https://onm.pages.dev/test1.html">a link 1</a>');
+document.write('<a href="https://onm.pages.dev/test2.html">a link 1</a>');
+document.write('<hr>');
+document.write('</center>');
+// End -->
