69baf8acc81caa9cc1df5a0c60df5a314dc6ab7b
Faiz-Liners <faizuddin0000@gmail.com>
2024-11-09 16:28:36 +0800

    parking.php

--- parking.php
+++ parking.php
@@ -3,5 +3,43 @@
 ##### https://github.com/AKPLAYLISTTV/iptv
 ##### https://github.com/BeritaSelatan/MalayINDO
 ##### https://github.com/Ramys/Iptv-Brasil-2024
-#####
-#####
+
+
+##### https://github.com/fawyrad/Premium-OTT ######
+
+Premium-OTT
+
+Letih asyik kena block dengan Janda OTT? Letih layan premium OTT beli dari seller tapi hot channels selalu down? Korang copy je link mpd bawah ni 👇
+
+    Guna selagi boleh
+    Link mpd Astro kena include clearkey dalam playlist korang since link adalah direct dari Astro Go bukan restream
+    Kalau tetiba takleh stream, maknanya account Astro dah kena blocked 👀
+    Sekiranya account Astro kena blocked, fix berlaku secara backend jadi link mpd bawah akan kekal sama
+    Open new issues untuk lapor sekiranya link mpd bawah ni DOWN
+
+15 Astro hot channels (24jam sentiasa ON)
+
+http://d1w72er2qn2fg9.cloudfront.net/ria.mpd
+http://d1w72er2qn2fg9.cloudfront.net/prima.mpd
+http://d1w72er2qn2fg9.cloudfront.net/warna.mpd
+http://d1w72er2qn2fg9.cloudfront.net/citra.mpd
+http://d1w72er2qn2fg9.cloudfront.net/vin.mpd
+http://d1w72er2qn2fg9.cloudfront.net/thang.mpd
+http://d1w72er2qn2fg9.cloudfront.net/aec.mpd
+http://d1w72er2qn2fg9.cloudfront.net/qj.mpd
+http://d1w72er2qn2fg9.cloudfront.net/aod.mpd
+http://d1w72er2qn2fg9.cloudfront.net/bola1.mpd
+http://d1w72er2qn2fg9.cloudfront.net/bola2.mpd
+http://d1w72er2qn2fg9.cloudfront.net/805.mpd
+http://d1w72er2qn2fg9.cloudfront.net/grand.mpd
+http://d1w72er2qn2fg9.cloudfront.net/premier1.mpd
+http://d1w72er2qn2fg9.cloudfront.net/premier2.mpd
+
+2 MYTV Mana-Mana FHD channels (24jam sentiasa ON)
+
+http://d1w72er2qn2fg9.cloudfront.net/bernama.m3u8
+http://d1w72er2qn2fg9.cloudfront.net/dw.m3u8
+
+
+
+###################################################
